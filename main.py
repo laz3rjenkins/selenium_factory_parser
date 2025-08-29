5702e0eb2daebb23f46bddd99dbc5c42bcf61218
Aleksey Milekhin <lesha.milehin@mail.ru>
2025-08-29 12:30:01 +0500

    tech

--- main.py
+++ main.py
@@ -16,12 +16,12 @@ def get_driver(headless: bool = False):
        """
     options = Options()
     if headless:
-        options.add_argument("--headless")
+        options.add_argument("--headless=new") 
         options.add_argument("--disable-gpu")
         options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--start-maximized")
     options.add_argument("--disable-extensions")
-    options.add_argument("--disable-dev-shm-usage")
 
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
